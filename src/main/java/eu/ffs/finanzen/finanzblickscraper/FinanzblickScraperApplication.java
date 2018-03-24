172552f806ff58cc99f8b1b1cc9406bd2874e7cb
jhi <johannes.hinkov@gmail.com>
2018-03-24 07:12:20 +0100

    Initial project setup

---
+++ src/main/java/eu/ffs/finanzen/finanzblickscraper/FinanzblickScraperApplication.java
@@ -0,0 +1,12 @@
+package eu.ffs.finanzen.finanzblickscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzblickScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzblickScraperApplication.class, args);
+	}
+}
